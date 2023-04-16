9e7456ece2f9d003313016389ed38c8528888ece
chris_achinga <achinga.chris@gmail.com>
2023-04-16 13:05:54 +0300

    get single product by ID

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const products  = require('./data/products');
 
 const app = express();
 
-// endpoint handler
+// endpoint handlers
+
+// default
 app.get('/', (req, res) => {
     res.send('AirspaceNext API is running --- ');
 });
 
+// get all products
 app.get('/products', (req, res) => {
     res.json(products);
+})
+
+// get single product
+app.get('/products/:id', (req, res) => {
+    const product = products.find((p) => p.id === req.params.id);
+    res.json(product);
 })
 
 app.listen(5000, console.log('Server started on port 5000'));
--- data/products.js
+++ data/products.js
@@ -1,7 +1,23 @@
 const products = [
     {
+        id: '1',
         name: 'Kenya Airways Boeing 787-8 Dreamliner',
-        image: '/images/airpods.jpg',
+        image: '/images/kq-787.jpg',
+        description:
+          'Extra Large Kenya Airways Boeing 787-8 Dreamliner Model, Heavy Duty',
+        aircraft_manufacturer: 'Boeing',
+        category: 'Airplane Models',
+        price: 120000,
+        scale: '1:150',
+        currency: 'KES',
+        countInStock: 10,
+        rating: 0,
+        numReviews: 0,
+      },
+      {
+        id: '2',
+        name: 'United Airlines Boeing 787-8 Dreamliner',
+        image: '/images/ua-787.jpg',
         description:
           'Extra Large Kenya Airways Boeing 787-8 Dreamliner Model, Heavy Duty',
         aircraft_manufacturer: 'Boeing',
